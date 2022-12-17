ff3768c01a71f32d6f7c7ba6e193baf99a9b81d4
hossameldinfahmi <hossameldinfahmi@gmail.com>
2022-12-17 17:23:16 +0200

    speical character / and *

--- checkSyntax.sh
+++ checkSyntax.sh
@@ -1,8 +1,14 @@
 #!/bin/bash 
-
+if [[ $1 == *"/"* ]]; then
+    return 0
+fi
+if [[ $1 == *"*"* ]]; then
+    return 0
+fi
 if [[ $1 == *['!'@#\$%^\&*()_+]* ]] || [[ $dbName == [0-9]* ]]; then
     return 0
     else
     return 1
 fi
+
 
